8cff1a5eb452731a4edade140d442f08a63d2548
misha-rozenczvit <153092800+misha-rozenczvit@users.noreply.github.com>
2023-12-10 14:39:46 +0300

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            {
+                //Console.WriteLine("Введи номер дня недели");
+                //byte day = byte.Parse(Console.ReadLine());
+                //switch (day)
+                //{
+                // case 1: Console.WriteLine("Понедельник");break;
+                // case 2: Console.WriteLine("Вторник");break;
+                // case 3: Console.WriteLine("Среда");break;
+                // case 4: Console.WriteLine("Четверг");break;
+                // case 5: Console.WriteLine("Пятница"); break;
+                // case 6: Console.WriteLine("Суббота");break;
+                // case 7: Console.WriteLine("Воскресенье"); break;
+
+
+
+
+
+
+                //}
+                //Console.ReadKey();
+
+                //Задание 2
+                //Console.WriteLine("Введите номер месяца");
+                //byte month = byte.Parse(Console.ReadLine());
+                //switch (month)
+                //{
+                // case 1: Console.WriteLine("Январь");break;
+                // case 2: Console.WriteLine("Февраль");break;
+                // case 3: Console.WriteLine("Март"); break;
+                // case 4: Console.WriteLine("Апрель"); break;
+                // case 5: Console.WriteLine("Май"); break;
+                // case 6: Console.WriteLine("Июнь"); break;
+                // case 7: Console.WriteLine("Июль"); break;
+                // case 8: Console.WriteLine("Август"); break;
+                // case 9: Console.WriteLine("Сентябрь"); break;
+                // case 10: Console.WriteLine("Октябрь"); break;
+                // case 11: Console.WriteLine("Ноябрь"); break;
+                // case 12: Console.WriteLine("Декабрь"); break;
+
+                //}
+                //Console.ReadKey();
+
+                //Задание 3
+                //Console.WriteLine("Введите название времени года");
+                //byte month = byte.Parse(Console.ReadLine());
+                //switch (month)
+                //{
+                // case 12: case 1: case 2:Console.WriteLine("Зима");break;
+                // case 3: case 4: case 5:Console.WriteLine("Весна");break;
+                // case 6: case 7: case 8:Console.WriteLine("Лето");break;
+                // case 9: case 10: case 11:Console.WriteLine("Осень");break;
+                // Console.ReadKey();
+                //}
+
+                //Задание 4
+                //Console.WriteLine("Введите название предмета");
+                //string lesson =(Console.ReadLine());
+                //switch (lesson)
+                //{
+                // case "ф":Console.WriteLine("физика");break;
+                // case "м":Console.WriteLine("математика");break;
+                // case "и":Console.WriteLine("история");break;
+                // case "г":Console.WriteLine("география");break;
+                // case "б":Console.WriteLine("биология");break;
+
+                //}
+                //Console.WriteLine();
+            }
+        }
+}
